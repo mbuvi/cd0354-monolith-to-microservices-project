379269026d450c10ad19a7d97655b1bb4d72effc
Steve Mbuvi <mbuvi.steve@gmail.com>
2022-09-22 13:17:23 +0300

    Updated secrets

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a4d58fcd5201c48f2aa384f88ad3db7e-1132260238.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
